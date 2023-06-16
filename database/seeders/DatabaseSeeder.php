4bd19887b1173b5731e55f7a4d9a7d43d00f1559
Camine123456 <carminefaella5@gmail.com>
2023-06-16 17:08:00 +0200

    Seeder generale

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RestaurantSedeer::class);
-        $this->call(TypeSedeer::class);
+        $this->call(RestaurantSeeder::class);
+        $this->call(TypeSeeder::class);
         $this->call(DishSeeder::class);
         
         // \App\Models\User::factory(10)->create();
