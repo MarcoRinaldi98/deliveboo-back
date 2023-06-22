8336645fcc1f348edd17548cd97fc2dcefae5604
Runatorium <stefano.alesci1@gmail.com>
2023-06-22 15:17:25 +0200

    bugfix

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -1,89 +1,86 @@
 @extends('layouts.admin')
 
 @section('content')
+    <form action="{{ route('admin.dishes.update', ['dish' => $dish->id]) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
 
-<form action="{{ route('admin.dishes.update',['dish' => $dish->id])}}" method="POST" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
-
-    <input type="hidden" name="restaurant_id" 
-    
-    @foreach ($restaurants as $restaurant)
+        <input type="hidden" name="restaurant_id"
+            @foreach ($restaurants as $restaurant)
         @if ($restaurant->user_id == auth()->user()->id)
             value="{{ $restaurant->id }}"
-        @endif
-    @endforeach
+        @endif @endforeach>
 
-    >    
-
-    <div class="mb-3">
-        <label for="name" class="form-label">Titolo</label>
-        <input type="text" class="form-control @error('name') is-invalid @enderror " id="name" name="name" value="{{old('name',$dish->name)}}">
-        @error('name')
-            <div class="invalid-feedback">
-                {{$message}}
-            </div>
-        @enderror
-    </div>
-    <div class="mb-3">
-        <label for="price" class="form-label">Prezzo</label>
-        <input type="text" class="form-control @error('price') is-invalid @enderror " id="price" name="price" value="{{old('price',$dish->price)}}">
-        @error('price')
-            <div class="invalid-feedback">
-                <div class="alert alert-danger">{{ $message }}</div>
-            </div>
-        @enderror
-    </div>
-
-    <div class="mb-3">
-        <label for="description" class="form-label">Descrizione</label>
-        <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{old('description', $dish->description)}}</textarea>
-        @error('description')
-            <div class="invalid-feedback">
-                <div class="alert alert-danger">{{ $message }}</div>
-            </div>
-        @enderror
-    </div>
-
-    <div class="mb-3">
-        <label for="available" class="form-label">Disponibile</label>
-        <select class="form-select @error('available') is-invalid @enderror" name="available" id="available">
-            <option @selected(old('available')== 0) value="0">No</option>
-            <option @selected(old('available')== 1) value="1">Si</option>
-        </select>
-        @error('available')
-            <div class="invalid-feedback">
-                <div class="alert alert-danger">{{ $message }}</div>
-            </div>
-        @enderror
-    </div>
-
-    <div class="mb-3">
-        <label for="image" class="form-label">Seleziona immagine di copertina</label>
-
-
-        @if ($dish->image)
-        <div class="my-img-wrapper">
-            <img class="img-thumbnail my-img-thumb" src="{{asset('storage/' . $dish->image)}}" alt="{{$dish->title}}"/>
-            <div id="btn-delete" class="my-img-delete btn btn-danger">X</div>
+        <div class="mb-3">
+            <label for="name" class="form-label">Titolo</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror " id="name" name="name"
+                value="{{ old('name', $dish->name) }}">
+            @error('name')
+                <div class="invalid-feedback">
+                    <div class="alert alert-danger">{{ $message }}</div>
+                </div>
+            @enderror
         </div>
-        @endif
+        <div class="mb-3">
+            <label for="price" class="form-label">Prezzo</label>
+            <input type="text" class="form-control @error('price') is-invalid @enderror " id="price" name="price"
+                value="{{ old('price', $dish->price) }}">
+            @error('price')
+                <div class="invalid-feedback">
+                    <div class="alert alert-danger">{{ $message }}</div>
+                </div>
+            @enderror
+        </div>
 
-        <input type="file" class="form-control @error('image') is-invalid @enderror " id="image" name="image">
+        <div class="mb-3">
+            <label for="description" class="form-label">Descrizione</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description', $dish->description) }}</textarea>
+            @error('description')
+                <div class="invalid-feedback">
+                    <div class="alert alert-danger">{{ $message }}</div>
+                </div>
+            @enderror
+        </div>
 
-        @error('image')
-            <div class="invalid-feedback">
-                <div class="alert alert-danger">{{ $message }}</div>
-            </div>
-        @enderror
-    </div>
+        <div class="mb-3">
+            <label for="available" class="form-label">Disponibile</label>
+            <select class="form-select @error('available') is-invalid @enderror" name="available" id="available">
+                <option @selected(old('available') == 0) value="0">No</option>
+                <option @selected(old('available') == 1) value="1">Si</option>
+            </select>
+            @error('available')
+                <div class="invalid-feedback">
+                    <div class="alert alert-danger">{{ $message }}</div>
+                </div>
+            @enderror
+        </div>
 
-    <button type="submit" class="btn btn-primary">Aggiorna Piatto</button>
-</form>
+        <div class="mb-3">
+            <label for="image" class="form-label">Seleziona immagine di copertina</label>
 
-<form id="form-delete" action="{{route('admin.dishes.deleteImage', ['id' => $dish->id])}}" method="POST">
-    @csrf
-    @method('DELETE')
-</form>
 
+            @if ($dish->image)
+                <div class="my-img-wrapper">
+                    <img class="img-thumbnail my-img-thumb" src="{{ asset('storage/' . $dish->image) }}"
+                        alt="{{ $dish->title }}" />
+                    <div id="btn-delete" class="my-img-delete btn btn-danger">X</div>
+                </div>
+            @endif
+
+            <input type="file" class="form-control @error('image') is-invalid @enderror " id="image" name="image">
+
+            @error('image')
+                <div class="invalid-feedback">
+                    <div class="alert alert-danger">{{ $message }}</div>
+                </div>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Aggiorna Piatto</button>
+    </form>
+
+    <form id="form-delete" action="{{ route('admin.dishes.deleteImage', ['id' => $dish->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+    </form>
 @endsection
