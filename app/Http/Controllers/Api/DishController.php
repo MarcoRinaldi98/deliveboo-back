c442b0faf248f35737bed19978aa2982a45460e1
Camine123456 <carminefaella5@gmail.com>
2023-06-28 11:51:10 +0200

    Modifiche al controller

--- app/Http/Controllers/Api/DishController.php
+++ app/Http/Controllers/Api/DishController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Dish;
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
 class DishController extends Controller
 {
@@ -21,11 +22,16 @@ class DishController extends Controller
 
     public function DishShow($restaurant_id)
     {
-        $dish = Dish::where('restaurant_id', $restaurant_id)->get();
+        $dishes = Dish::where('restaurant_id', $restaurant_id)->get();
+
+        $restaurant = Restaurant::find($restaurant_id);
 
         return response()->json([
             'success' => true,
-            'results' => $dish
+            'results' => [
+                'dishes' => $dishes,
+                'restaurant' => $restaurant
+            ]
         ]);
     }
 }
