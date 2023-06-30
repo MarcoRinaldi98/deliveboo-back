d8dce23b6d3c76aad5af37dafed4df5a9918bd11
Camine123456 <carminefaella5@gmail.com>
2023-06-30 17:35:37 +0200

    Finito pagamento

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,44 +5,62 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
-
+use Braintree\Transaction;
+use Braintree\Configuration;
+ 
 class OrderController extends Controller
 {
     public function store(Request $request)
     {
-    
         $validatedData = $request->validate([
             'guest_name' => 'required',
             'guest_surname' => 'required',
             'guest_address' => 'required',
             'guest_email' => 'required|email',
             'guest_phone' => 'required',
-            'amount'=>'required',
-            'status'=>'required',
-            'date'=>'required',
-            'restaurant_id'=>'required',
-            'nonce'=>'nullable',
+            'amount' => 'required',
+            'status' => 'required',
+            'date' => 'required',
+            'restaurant_id' => 'required',
+            'nonce' => 'nullable',
         ]);
 
-        $order = Order::create([
-            'guest_name' => $validatedData['guest_name'],
-            'guest_surname' => $validatedData['guest_surname'],
-            'guest_address' => $validatedData['guest_address'],
-            'guest_email' => $validatedData['guest_email'],
-            'guest_phone' => $validatedData['guest_phone'],
+        Configuration::environment('sandbox'); 
+        Configuration::merchantId('c3ksmy3jwrcb35jv'); 
+        Configuration::publicKey('2qbxs4rtwhz8kkhz'); 
+        Configuration::privateKey('6633e8a5d2695c41b7dc163d9d33e52b'); 
+
+        $nonce = $validatedData['nonce'];
+
+        $result = Transaction::sale([
             'amount' => $validatedData['amount'],
-            'status' => $validatedData['status'],
-            'date' => $validatedData['date'],
-            'restaurant_id' => $validatedData['restaurant_id'],
-            'nonce' => $validatedData['nonce'],
+            'paymentMethodNonce' => $nonce,
+            'options' => [
+                'submitForSettlement' => true
+            ]
         ]);
 
-        // Puoi eseguire altre operazioni qui, come inviare notifiche o aggiornare altre tabelle nel database
+        if ($result->success) {
+            $order = Order::create([
+                'guest_name' => $validatedData['guest_name'],
+                'guest_surname' => $validatedData['guest_surname'],
+                'guest_address' => $validatedData['guest_address'],
+                'guest_email' => $validatedData['guest_email'],
+                'guest_phone' => $validatedData['guest_phone'],
+                'amount' => $validatedData['amount'],
+                'status' => $validatedData['status'],
+                'date' => $validatedData['date'],
+                'restaurant_id' => $validatedData['restaurant_id'],
+                'nonce' => $validatedData['nonce'],
+            ]);
 
-        // Restituzione della risposta
-        return response()->json([
-            'message' => 'Order created successfully',
-            'order' => $order,
-        ], 201);
+            return response()->json([
+                'message' => 'Order created successfully',
+                'order' => $order,
+            ], 201);
+        } else {
+            $error = $result->message;
+            return response()->json(['message' => $error, 'order' => false]);
+        }
     }
 }
--- config/braintree.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return[
-    'merchantId' => env('BRAINTREE_MERCHANT_ID'),
-    'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-    'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
-];
